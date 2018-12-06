521b622398b4e65840bc8a698c9a932527e6782e
Axel Petit <axel.petit@epita.fr>
2018-12-06 15:02:28 +0100

    [EVAL] Add project1 network, test with different word2vec

---
+++ src/main_seval.py
@@ -0,0 +1,300 @@
+import numpy as np
+from keras.preprocessing.text import Tokenizer
+from keras.preprocessing.sequence import pad_sequences
+from keras.utils import to_categorical
+from keras.models import Sequential
+from keras.layers import Input, Dense, Embedding, LSTM, Concatenate, Reshape, GRU, Bidirectional, Dropout
+import numpy as np
+import keras
+from preprocessing_git import data_preprocessing, standardization
+from gensim.models import KeyedVectors
+from keras.utils.np_utils import to_categorical
+import pickle
+import pandas as pd
+from scipy.stats import pearsonr
+from keras.layers.embeddings import Embedding
+from keras.models import Sequential, Model
+from keras.layers import LSTM, Dropout, Dense, Activation, Bidirectional,  Flatten, Input, GRU, GaussianNoise
+from keras.models import load_model
+from kutilities.layers import Attention
+from keras.optimizers import Adam
+from keras.regularizers import l2
+from keras.preprocessing.sequence import pad_sequences
+from keras import optimizers
+from keras.models import load_model
+import json, argparse, os
+import re
+import pandas as pd
+import io
+import sys
+from keras.models import Model
+from train import get_max_len, createEmbedingMatrix
+from preprocessing_git import data_preprocessing_teacher
+
+# Path to training and testing data file. This data can be downloaded from a link, details of which will be provided.
+trainDataPath = ""
+testDataPath = ""
+# Output file that will be generated. This file can be directly submitted.
+solutionPath = ""
+# Path to directory where GloVe file is saved.
+gloveDir = ""
+
+NUM_FOLDS = 2                   # Value of K in K-fold Cross Validation
+NUM_CLASSES = 4                 # Number of classes - Happy, Sad, Angry, Others
+MAX_NB_WORDS = None                # To set the upper limit on the number of tokens extracted using keras.preprocessing.text.Tokenizer 
+MAX_SEQUENCE_LENGTH = None         # All sentences having lesser number of words than this will be padded
+EMBEDDING_DIM = 344               # The dimension of the word embeddings
+BATCH_SIZE = 200                  # The batch size to be chosen for training the model.
+LSTM_DIM = 300                    # The dimension of the representations learnt by the LSTM model
+DROPOUT = 0.2                     # Fraction of the units to drop for the linear transformation of the inputs. Ref - https://keras.io/layers/recurrent/
+NUM_EPOCHS = 10                  # Number of epochs to train a model for
+LEARNING_RATE = 0.001
+
+
+from preprocessing_git import data_preprocessing_semeval
+def buildModel(embeddingMatrix, MAX_SEQUENCE_LENGTH):
+    """Constructs the architecture of the model
+    Input:
+        embeddingMatrix : The embedding matrix to be loaded in the embedding layer.
+    Output:
+        model : A basic LSTM model
+    """
+    x1 = Input(shape=(MAX_SEQUENCE_LENGTH,), dtype='int32', name='main_input1')
+    x2 = Input(shape=(MAX_SEQUENCE_LENGTH,), dtype='int32', name='main_input2')
+    x3 = Input(shape=(MAX_SEQUENCE_LENGTH,), dtype='int32', name='main_input3')
+
+    embeddingLayer = Embedding(embeddingMatrix.shape[0],
+                                EMBEDDING_DIM,
+                                weights=[embeddingMatrix],
+                                input_length=MAX_SEQUENCE_LENGTH,
+                                trainable=False)
+    emb1 = embeddingLayer(x1)
+    emb2 = embeddingLayer(x2)
+    emb3 = embeddingLayer(x3)
+
+    lstm = Bidirectional(LSTM(LSTM_DIM, dropout=DROPOUT))
+
+    lstm1 = lstm(emb1)
+    lstm2 = lstm(emb2)
+    lstm3 = lstm(emb3)
+
+    inp = Concatenate(axis=-1)([lstm1, lstm2, lstm3])
+
+    inp = Reshape((3, 2*LSTM_DIM, )) (inp)
+
+    lstm_up = LSTM(LSTM_DIM, dropout=DROPOUT)
+
+    out = lstm_up(inp)
+
+    out = Dense(NUM_CLASSES, activation='softmax')(out)
+    
+    adam = optimizers.adam(lr=LEARNING_RATE)
+    model = Model([x1,x2,x3],out)
+    model.compile(loss='categorical_crossentropy',
+                  optimizer=adam,
+                  metrics=['acc'])
+    print(model.summary())
+    return model
+
+def model(x_train_3, y_train_3, x_val_3, y_val_3, embedding_layer):
+    model2 = Sequential()
+    model2.add(embedding_layer)
+    model2.add(GaussianNoise(0.3))
+    model2.add(Dropout(0.3))
+    model2.add(Bidirectional(LSTM(150, recurrent_dropout=0.3, kernel_regularizer=l2(0), return_sequences=True)))
+    model2.add(Dropout(0.3))
+    model2.add(Bidirectional(LSTM(150, recurrent_dropout=0.3, kernel_regularizer=l2(0), return_sequences=True)))
+    model2.add(Dropout(0.3))
+    model2.add(Attention())
+    model2.add(Dense(4, activity_regularizer=l2(0.0001)))
+    model2.add(Activation('softmax'))
+    model2.compile(optimizer=Adam(clipnorm=1, lr=0.001),
+                  loss='categorical_crossentropy',
+                  metrics=['acc'])
+    model2.summary()
+    model2.fit(x_train_3, y_train_3, validation_data=(x_val_3, y_val_3),epochs=12, batch_size=50)
+    model2.save("./model_test.h5")
+
+
+def model1(x_train, y_train, x_val, y_val, embedding_layer):
+
+  model1 = Sequential()
+  model1.add(embedding_layer)
+  model1.add(Dropout(0.5))
+  model1.add(GRU(128))
+  model1.add(Dropout(0.5))
+  model1.add(Dense(32, activation='relu'))
+  model1.add(Dropout(0.2))
+  model1.add(Dense(4, activation='softmax'))
+  model1.compile(loss='categorical_crossentropy',
+            optimizer='Adam',
+            metrics=['acc'])
+  model1.summary()
+  model1.fit(x_train, y_train, validation_data=(x_val, y_val), batch_size=50, epochs=3,  verbose=1)
+  model1.save("./semeval_teacher_bis.h5")
+
+  return model1
+
+def phd_def_train():
+    trainIndices, u1_train, u2_train, u3_train, labels  = data_preprocessing_semeval('../resources/train.txt', 'train')
+    print("Extracting tokens...")
+    tokenizer = Tokenizer()
+    tokenizer.fit_on_texts(u1_train + u2_train + u3_train)
+    u1_trainSequences, u2_trainSequences, u3_trainSequences = tokenizer.texts_to_sequences(u1_train), tokenizer.texts_to_sequences(u2_train), tokenizer.texts_to_sequences(u3_train)
+    u1_testSequences, u2_testSequences, u3_testSequences = tokenizer.texts_to_sequences(u1_test), tokenizer.texts_to_sequences(u2_test), tokenizer.texts_to_sequences(u3_test)
+
+    MAX_SEQUENCE_LENGTH = get_max_len([u1_train, u2_train, u3_train], tokenizer)
+
+    wordIndex = tokenizer.word_index
+    print("Found %s unique tokens." % len(wordIndex))
+
+    print("Populating embedding matrix...")
+    embedding_matrix = createEmbedingMatrix(wordIndex, '../resources/model2.kv', EMBEDDING_DIM)
+
+    u1_data = pad_sequences(u1_trainSequences, maxlen=MAX_SEQUENCE_LENGTH)
+    u2_data = pad_sequences(u2_trainSequences, maxlen=MAX_SEQUENCE_LENGTH)
+    u3_data = pad_sequences(u3_trainSequences, maxlen=MAX_SEQUENCE_LENGTH)
+    labels = to_categorical(np.asarray(labels))
+
+    np.random.shuffle(trainIndices)
+
+    u1_data = u1_data[trainIndices]
+    u2_data = u2_data[trainIndices]
+    u3_data = u3_data[trainIndices]
+
+
+
+    labels = labels[trainIndices]
+
+
+        # Perform k-fold cross validation
+    metrics = {"accuracy" : [],
+                "microPrecision" : [],
+                "microRecall" : [],
+                "microF1" : []}
+
+    model = buildModel(embedding_matrix, MAX_SEQUENCE_LENGTH)
+
+    model.fit([u1_data, u2_data, u3_data], labels,  epochs=NUM_EPOCHS, batch_size=BATCH_SIZE)
+    model.save('semeval_phd.h5')
+
+def teacher_def_train():
+    x_train, y_train = data_preprocessing_teacher('../resources/train.txt', 'True')
+    x_test = data_preprocessing_teacher('../resources/test.txt', 'False')
+    all_tweet = x_train.append(x_test)
+    tokenizer = Tokenizer(filters=' ')
+    tokenizer.fit_on_texts(all_tweet)
+    word_index = tokenizer.word_index
+    sequences_train = tokenizer.texts_to_sequences(x_train)
+    sequences_test = tokenizer.texts_to_sequences(x_test)
+    MAX_SEQUENCE_LENGTH = get_max_len([x_test, x_train], tokenizer)
+    data_train = pad_sequences(sequences_train, maxlen=MAX_SEQUENCE_LENGTH)
+    data_test = pad_sequences(sequences_test, maxlen=MAX_SEQUENCE_LENGTH)
+    indices_train = np.arange(data_train.shape[0])
+    data_train = data_train[indices_train]
+    indices_test = np.arange(data_test.shape[0])
+    data_test = data_test[indices_test]
+    
+    nb_words=len(word_index)+1
+
+    y_train = to_categorical(np.asarray(y_train), 4)
+    embedding_matrix = createEmbedingMatrix(word_index, '../resources/model2.kv', EMBEDDING_DIM)
+    
+    embedding_layer = Embedding(nb_words,
+                            EMBEDDING_DIM,
+                            weights=[embedding_matrix],
+                            input_length=MAX_SEQUENCE_LENGTH,
+                            trainable=True, name='embedding_layer')
+    
+    split_idx = int(len(x_train)*0.80)
+    x_train, x_val = data_train[:split_idx], data_train[split_idx:]
+    y_train, y_val = y_train [:split_idx], y_train[split_idx:]
+    model1(x_train, y_train, x_val, y_val, embedding_layer)
+
+
+def validation_teacher(modelPath):
+    x_train, y_train = data_preprocessing_teacher('../resources/train.txt', 'True')
+    x_test= data_preprocessing_teacher('../resources/test.txt', 'False')
+
+    all_tweet = x_train.append(x_test)
+
+    tokenizer = Tokenizer(filters=' ')
+    tokenizer.fit_on_texts(all_tweet)
+    word_index = tokenizer.word_index
+
+
+    sequences_train = tokenizer.texts_to_sequences(x_train)
+    sequences_test = tokenizer.texts_to_sequences(x_test)
+
+    MAX_SEQUENCE_LENGTH = get_max_len([x_train, x_test], tokenizer)
+    sequences = sequences_train + sequences_test
+    data_train = pad_sequences(sequences_train, maxlen=MAX_SEQUENCE_LENGTH)
+    data_test = pad_sequences(sequences_test, maxlen=MAX_SEQUENCE_LENGTH)
+
+    indices_train = np.arange(data_train.shape[0])
+    data_train = data_train[indices_train]
+
+    indices_test = np.arange(data_test.shape[0])
+    data_test = data_test[indices_test]
+
+    f = open("./test.txt", "w")
+    f.write("id\tturn1\tturn2\tturn3\tlabel\n")
+    nn_model=load_model(modelPath, custom_objects={"Attention": Attention})
+    r = nn_model.predict(data_test)
+    print(r)
+    data = pd.read_csv("../resources/test.txt", sep='\t', encoding='utf-8',     names=['id','turn1','turn2','turn3'])
+    
+    for d in range(1,len(data)):
+        i=d-1 
+        idx=np.argmax(r[i])
+        if (idx==0):
+            label="angry"
+        elif(idx==1):
+            label="happy"
+        elif(idx==3):
+            label="others"
+        elif(idx==2):
+            label="sad"
+        f.write(str(data["id"][d])+"\t"+str(data["turn1"][d])+"\t"+str(data["turn2"][d])+"\t"+str(data["turn3"][d])+"\t"+label+"\n")
+    # print(str(data["id"][d])+"\t"+str(data["turn1"][d])+"\t"+str(data["turn2"][d])+"\t"+str (data["turn3"][d])+"\t"+label)
+    f.close()
+
+def validation_phd(modelFile):
+    print("Processing training data...")
+    train_indices, u1_train, u2_train, u3_train, labels = data_preprocessing_semeval('../resources/train.txt', 'train')
+    print("Processing test data...")
+    test_indices, u1_test, u2_test, u3_test = data_preprocessing_semeval('../resources/test.txt', 'test')
+
+    print("Extracting tokens...")
+    tokenizer = Tokenizer()
+    tokenizer.fit_on_texts(u1_train + u2_train + u3_train)
+    u1_trainSequences, u2_trainSequences, u3_trainSequences = tokenizer.texts_to_sequences(u1_train), tokenizer.texts_to_sequences(u2_train), tokenizer.texts_to_sequences(u3_train)
+    u1_testSequences, u2_testSequences, u3_testSequences = tokenizer.texts_to_sequences(u1_test), tokenizer.texts_to_sequences(u2_test), tokenizer.texts_to_sequences(u3_test)
+    MAX_SEQUENCE_LENGTH = get_max_len([u1_train, u2_train, u3_train], tokenizer)
+
+    wordIndex = tokenizer.word_index
+    print("Found %s unique tokens." % len(wordIndex))
+
+    print("Populating embedding matrix...")
+    embedding_matrix = createEmbedingMatrix(wordIndex, '../resources/model2.kv', EMBEDDING_DIM)
+
+    u1_data = pad_sequences(u1_trainSequences, maxlen=MAX_SEQUENCE_LENGTH)
+    u2_data = pad_sequences(u2_trainSequences, maxlen=MAX_SEQUENCE_LENGTH)
+    u3_data = pad_sequences(u3_trainSequences, maxlen=MAX_SEQUENCE_LENGTH)
+
+    u1_testData, u2_testData, u3_testData = pad_sequences(u1_testSequences, maxlen=MAX_SEQUENCE_LENGTH), pad_sequences(u2_testSequences, maxlen=MAX_SEQUENCE_LENGTH), pad_sequences(u3_testSequences, maxlen=MAX_SEQUENCE_LENGTH)
+    model = load_model(modelFile)
+    model.summary()
+    predictions = model.predict([u1_testData, u2_testData, u3_testData], batch_size=50)
+    predictions = predictions.argmax(axis=1)
+    with io.open('solution_teacher.csv', "w", encoding="utf8") as fout:
+            fout.write('\t'.join(["id", "turn1", "turn2", "turn3", "label"]) + '\n')        
+            with io.open('../resources/test.txt', encoding="utf8") as fin:
+                fin.readline()
+                for lineNum, line in enumerate(fin):
+                    fout.write('\t'.join(line.strip().split('\t')[:4]) + '\t')
+                    fout.write(label2emotion[predictions[lineNum]] + '\n')
+            print('Completed. Model parameters: ')
+
+#teacher_def_train()
+validation_teacher('./semeval_teacher_bis.h5')
--- src/preprocessing_git.py
+++ src/preprocessing_git.py
@@ -7,6 +7,7 @@ from nltk.tokenize import TweetTokenizer
 from string import punctuation
 from nltk.corpus import stopwords
 import pandas as pd
+import re
 tknzr = TweetTokenizer()
 
 
@@ -36,6 +37,10 @@ text_processor = TextPreProcessor(
 lemmatizer = WordNetLemmatizer()
 
 
+
+emotion2label = {"others":0, "happy":1, "sad":2, "angry":3}
+emotion2label_teacher = {"others":3, "happy":1, "sad":2, "angry":0}
+
 def data_preprocessing(path_tweets,corpora):
 	data = pd.read_csv(path_tweets, encoding='utf-8',sep='\t', names=['id','class','text'])
 	if corpora=='train':
@@ -43,11 +48,73 @@ def data_preprocessing(path_tweets,corpora):
 	data['text'] = data['text'].apply(lambda x: standardization(x))
 	return data['text'], data['class']
 
+def data_preprocessing_semeval(path_data, corpora):
+        data = pd.read_csv(path_data, encoding='utf-8', sep='\t')
+        data['turn1'] = data['turn1'].apply(lambda x: standardization(x))
+        data['turn2'] = data['turn2'].apply(lambda x: standardization(x))
+        data['turn3'] = data['turn3'].apply(lambda x: standardization(x))
+        if corpora=='train':
+                data['label'] = data['label'].apply(lambda x: emotion2label[x])
+                return data['id'], data['turn1'], data['turn2'], data['turn3'], data['label']
+        return data['id'], data['turn1'], data['turn2'], data['turn3']
+
+def load_data_semeval(path_tweet, label):
+	try:
+		if label=='True':
+			tweet = pd.read_csv(path_tweet, encoding='utf-8',sep='\t')
+			text = tweet['turn1']+" "+tweet['turn2']+" "+tweet['turn3']
+			return text, tweet['label']
+		else:
+			tweet = pd.read_csv(path_tweet, encoding='utf-8',sep='\t')
+			text = tweet['turn1']+" "+tweet['turn2']+" "+tweet['turn3']
+			return text			
+	except IOError: 
+		print ("Could not read file:", path_tweet)
+
+
+def data_preprocessing_teacher(path_tweet, label):
+	if label=='True':
+		texts, labels = load_data_semeval(path_tweet,label)
+		texts = texts.apply(lambda x: standardization(x))
+		labels = labels.apply(lambda x: emotion2label_teacher[x])	
+		return texts, labels
+	else:
+		texts = load_data_semeval(path_tweet,label)
+		texts = texts.apply(lambda x: standardization(x))	
+		return texts
+
+def standardization_teacher(tweet):
+	tweet = re.sub(r"\\u2019", "'", tweet)
+	tweet = re.sub(r"\\u002c", ",", tweet)
+	tweet=emoji.str2emoji(tweet)
+	tweet = re.sub(r"(http|https)?:\/\/[a-zA-Z0-9\.-]+\.[a-zA-Z]{2,4}(/\S*)?", " ", tweet)
+	tweet = re.sub(r"u r "," you are ",tweet)
+	tweet = re.sub(r"U r "," you are ",tweet)
+	tweet = re.sub(r" u(\s|$)"," you ",tweet)
+	tweet = re.sub(r"didnt","did not",tweet)
+	tweet = re.sub(r"\'ve", " have", tweet)
+	tweet = re.sub(r" can\'t", " cannot", tweet)
+	tweet = re.sub(r"n\'t", " not", tweet)
+	tweet = re.sub(r"\'re", " are", tweet)
+	tweet = re.sub(r"\'d", " would", tweet)
+	tweet = re.sub(r"\'ll", " will", tweet)
+	tweet = re.sub(r"\'s", "", tweet)
+	tweet = re.sub(r"\'n", "", tweet)
+	tweet = re.sub(r"\'m", " am", tweet)
+	tweet = re.sub(r"@\w+", r' ',tweet)
+	tweet = re.sub(r"#\w+", r' ',tweet)
+	tweet = re.sub(r" [0-9]+ "," ",tweet)
+	tweet = re.sub(r" plz[\s|$]", " please ",tweet)
+	tweet = re.sub(r"^([1-9] |1[0-9]| 2[0-9]|3[0-1])(.|-)([1-9] |1[0-2])(.|-|)20[0-9][0-9]"," ",tweet)
+	tweet = [lemmatizer.lemmatize(i,j[0].lower()) if j[0].lower() in ['a','n','v']  else lemmatizer.lemmatize(i) for i,j in pos_tag(tknzr.tokenize(tweet))]
+	tweet = [ i for i in tweet if (i not in stopwords) and (i not in punctuation ) ]
+	tweet = ' '.join(tweet)
+	return tweet.lower()
+
 
 def standardization(tweet):
     tweet=' '.join(emoji.str2emoji(tweet.split()))
     tweets = text_processor.pre_process_doc(tweet)
-    tweets = emoji.str2emoji(tweets)
     tweets = [lemmatizer.lemmatize(word,grammar[0].lower()) if grammar[0].lower() in ['a','n','v']  else lemmatizer.lemmatize(word) for word,grammar in pos_tag(tweets)]
     tweets = [tweet for tweet in tweets if (tweet not in punctuation) and (tweet not in stopwords)]
     return tweets
